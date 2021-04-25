2a1caab84f0791d5be6bcc6c5864f9b012c864c0
Mauritz Marais <mauritz303@gmail.com>
2021-04-25 11:04:03 +0200

    added reset button with prompt

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const mainDiv = document.querySelector('.container')
+const mainDiv = document.querySelector('.container');
+const button = document.querySelector('button');
+
 let div;
 let small_div;
+let num;
 
-function createSquare() {
-    for (let i = 0; i < 16; i++) {
+function createSquare(num) {
+    for (let i = 0; i < num; i++) {
         div = document.createElement('div');
         div.classList.add('medium-div');
         mainDiv.appendChild(div);
-        for (let j = 0; j < 16; j++) {
+        for (let j = 0; j < num; j++) {
             small_div = document.createElement('div');
             small_div.classList.add('small-div')
             div.appendChild(small_div);
             small_div.addEventListener('mouseenter', (e) => {
-                e.target.style.background = 'black';
+                e.target.style.backgroundColor = 'black';
             });
 
         }
@@ -20,5 +23,18 @@ function createSquare() {
     }
 }
 
-createSquare();
+createSquare(16);
 
+button.addEventListener('click', (e) => {
+    let first = mainDiv.firstElementChild;
+    while (first) {
+        first.remove();
+        first = mainDiv.firstElementChild
+    }
+    inputSquares()
+});
+
+function inputSquares(num) {
+    num = parseInt(prompt('How many squares per side? '));
+    createSquare(num)
+}
